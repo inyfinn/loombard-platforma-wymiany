db13a5f19c164da6ff8932c282602877c86ba57c
inyfinn <xpretrax@gmail.com>
2025-07-29 21:11:31 +0200

    fix: set production base path to /loombard-platforma-wymiany/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/kantoor-platforma-wymiany/' : '/',
+  base: mode === 'production' ? '/loombard-platforma-wymiany/' : '/',
   server: {
     host: "::",
     port: 8080,
